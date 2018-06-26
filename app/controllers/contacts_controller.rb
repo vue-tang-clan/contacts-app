81001ac01e8669ef6b4999437703eae05df05f4d
Peter Jang <peter.x.jang@gmail.com>
2018-06-25 21:05:44 -0500

    Add all contacts action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,12 @@
 class ContactsController < ApplicationController
   def one_contact_method
     @contact = Contact.first
+    @contact.email
     render "one_contact.json.jbuilder"
+  end
+
+  def all_contacts_method
+    @contacts = Contact.all
+    render "all_contacts.json.jbuilder"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/one_contact_url" => "contacts#one_contact_method"
+  get "/all_contacts_url" => "contacts#all_contacts_method"
 end
