b6b3a3442f105686d368dfde57ee11454cbcf360
Peter Jang <peter.x.jang@gmail.com>
2018-06-29 13:03:19 -0500

    Add RESTful contacts update action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -19,4 +19,14 @@ class ContactsController < ApplicationController
     @contact = Contact.find_by(id: params[:id])
     render "show.json.jbuilder"
   end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.save
+    render "show.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/contacts" => "contacts#index"
   post "/contacts" => "contacts#create"
   get "/contacts/:id" => "contacts#show"
+  patch "/contacts/:id" => "contacts#update"
 end
