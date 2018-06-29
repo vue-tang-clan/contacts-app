a49925713981333f132124774d6f392e669b745d
Peter Jang <peter.x.jang@gmail.com>
2018-06-29 10:28:57 -0500

    Make existing actions RESTful contacts index and show actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,11 @@
 class ContactsController < ApplicationController
-  def one_contact_method
-    @contact = Contact.first
-    @contact.email
-    render "one_contact.json.jbuilder"
+  def index
+    @contacts = Contact.all
+    render "index.json.jbuilder"
   end
 
-  def all_contacts_method
-    @contacts = Contact.all
-    render "all_contacts.json.jbuilder"
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.json.jbuilder"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get "/one_contact_url" => "contacts#one_contact_method"
-  get "/all_contacts_url" => "contacts#all_contacts_method"
+  get "/contacts" => "contacts#index"
+  get "/contacts/:id" => "contacts#show"
 end
