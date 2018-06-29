7fc592474d28f1442bfb8c398a16c7b7c785655a
Peter Jang <peter.x.jang@gmail.com>
2018-06-29 10:35:28 -0500

    Add RESTful contacts create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,17 @@ class ContactsController < ApplicationController
     render "index.json.jbuilder"
   end
 
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    @contact.save
+    render "show.json.jbuilder"
+  end
+
   def show
     @contact = Contact.find_by(id: params[:id])
     render "show.json.jbuilder"
